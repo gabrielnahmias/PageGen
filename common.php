b25aa852a9d76a827d1f8e9a80c8c39b6618e2e2
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-11-11 13:32:09 -0600

    Added Smarty and additional comments.

--- common.php
+++ common.php
@@ -1,16 +1,23 @@
 <?php
-
 require_once "config.php";
+
+require_once DIR_SMARTY."/Smarty.class.php";
+
 require_once DIR_CLS."/Browser/Browser.php";
 require_once DIR_CLS."/Utils.php";
 require_once DIR_CLS."/Web.php";
 
+$smarty = new Smarty();
 $br = new Browser();
+
+$smarty->setTemplateDir(DIR_ASSETS."/tpl");
 
 // Normalize some commonly referred-to values.
 $browser = $br->getBrowserArray();
 
-$bI = $browser['iOS'] = ( substr($br->getPlatform(), 0, 2) == "iP" ) ? true : false;
+// Two handy variables (a single variable for some and an array element inside the browser array
+// for the organized).
+$iPhone = $browser['iOS'] = ( substr($br->getPlatform(), 0, 2) == "iP" ) ? true : false;
 
 // Possible future overrides:
 //$browser['classString'] = $br->getClassString("", true, true, true, true);
